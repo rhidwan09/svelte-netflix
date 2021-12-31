44c2a928b8d537bddf10328709723581389ee396
rhidwan09 <muhammadrhidwan147@gmail.com>
2021-12-31 17:07:32 +0700

    update icon

--- src/lib/svg-loader/index.js
+++ src/lib/svg-loader/index.js
@@ -1,29 +1,29 @@
-export { default as DolbyIcon } from './icons/DolbyIcon.svelte';
+export { default as DolbyIcon } from './Icons/DolbyIcon.svelte';
 
-export { default as DolbyVerticalIcon } from './icons/DolbyVerticalIcon.svelte';
+export { default as DolbyVerticalIcon } from './Icons/DolbyVerticalIcon.svelte';
 
-export { default as NetflixIcon } from './icons/NetflixIcon.svelte';
+export { default as NetflixIcon } from './Icons/NetflixIcon.svelte';
 
-export { default as NetflixVerticalIcon } from './icons/NetflixVerticalIcon.svelte';
+export { default as NetflixVerticalIcon } from './Icons/NetflixVerticalIcon.svelte';
 
-export { default as SearchIcon } from './icons/SearchIcon.svelte';
+export { default as SearchIcon } from './Icons/SearchIcon.svelte';
 
-export { default as ChevronDownIcon } from './icons/ChevronDownIcon.svelte';
+export { default as ChevronDownIcon } from './Icons/ChevronDownIcon.svelte';
 
-export { default as MenuIcon } from './icons/MenuIcon.svelte';
+export { default as MenuIcon } from './Icons/MenuIcon.svelte';
 
-export { default as CloseIcon } from './icons/CloseIcon.svelte';
+export { default as CloseIcon } from './Icons/CloseIcon.svelte';
 
-export { default as ChevronUpIcon } from './icons/ChevronUpIcon.svelte';
+export { default as ChevronUpIcon } from './Icons/ChevronUpIcon.svelte';
 
-export { default as SearchColor } from './icons/SearchColor.svelte';
+export { default as SearchColor } from './Icons/SearchColor.svelte';
 
 export { default as StarIcon } from './Icons/StarIcon.svelte';
 
-export { default as PlayOnlineIcon } from './icons/PlayOnlineIcon.svelte';
+export { default as PlayOnlineIcon } from './Icons/PlayOnlineIcon.svelte';
 
-export { default as ChevronRightIcon } from './icons/ChevronRightIcon.svelte';
+export { default as ChevronRightIcon } from './Icons/ChevronRightIcon.svelte';
 
-export { default as ChevronLeftIcon } from './icons/ChevronLeftIcon.svelte';
+export { default as ChevronLeftIcon } from './Icons/ChevronLeftIcon.svelte';
 
-export { default as PolygonIcon } from './icons/PolygonIcon.svelte';
+export { default as PolygonIcon } from './Icons/PolygonIcon.svelte';
